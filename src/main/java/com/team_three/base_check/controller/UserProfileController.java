4d4f149074d2045fddee3e2a825303e3ecadc516
Charles_Yang <charles_yang.email@foxmail.com>
2022-06-11 12:43:15 +0800

    多余项删除

--- src/main/java/com/team_three/base_check/controller/UserProfileController.java
+++ src/main/java/com/team_three/base_check/controller/UserProfileController.java
@@ -1,6 +1,5 @@
 package com.team_three.base_check.controller;
 
-import com.team_three.base_check.pojo.User;
 import com.team_three.base_check.pojo.UserProfile;
 import com.team_three.base_check.service.impl.UserProfileServiceImpl;
 import org.apache.shiro.SecurityUtils;
@@ -39,7 +38,6 @@ public class UserProfileController {
     @Resource
     private UserProfileServiceImpl userProfileServiceImpl;
 
-
     @RequestMapping(value = "/selectUser", method = RequestMethod.GET)
     public ModelAndView selectUser(@RequestParam(value = "msg", required = false) String msg, Model model) {
         Subject subject = SecurityUtils.getSubject();
@@ -52,74 +50,7 @@ public class UserProfileController {
             model.addAttribute("msg",msg);
         }
         model.addAttribute("profile",userProfile);
-        ModelAndView mv = new ModelAndView("user-profile");
-        return mv;
-    }
-
-    /**
-     * 通过主键查询单条数据
-     * @param page  需要查询的页码
-     * @param name  模糊查询的内容
-     * @return 单条数据
-     */
-    @RequestMapping(value = "/selectByPage", method = RequestMethod.GET)
-    @ApiOperation(value = "列表查询分页接口",notes = "列表查询分页接口",httpMethod = "GET")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "page",value = "页码",paramType = "query",dataType = "String",required = true),
-            @ApiImplicitParam(name = "name",value = "**名称",paramType = "query",dataType = "String",required = false)
-    })
-    public Map<String, Object> selectByPage(@RequestParam(name = "page") int page, String name) {
-        return this.userProfileServiceImpl.selectByPage(page, name);
-    }
-    
-    /**
-     * 查询所有数据
-     * @return 返回所有数据
-     */
-    @RequestMapping(value = "/selectAll", method = RequestMethod.GET)
-    @ApiOperation(value = "查询所有数据接口",notes = "查询所有数据接口",httpMethod = "GET")
-    public Map<String, Object> selectAll() {
-        return this.userProfileServiceImpl.selectAll();
-    }
-    
-    /**
-     * 通过主键删除单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
-     */
-//    @RequestMapping(value = "/shanChuById", method = RequestMethod.POST)
-//    @ApiOperation(value = "根据一个主键删除一条**记录的接口",notes = "根据一个主键删除一条**记录的接口",httpMethod = "POST")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "id",value = "主键",paramType = "query",dataType = "String",required = true),
-//    })
-    public Map<String, Object> deleteById(@RequestParam("id") String id) {
-        return this.userProfileServiceImpl.deleteById(id);
-    }
-
-    
-    /**
-     * 通过主键更新单条数据
-     *
-     * @param userProfile 一个数据库对应的POJO数据对象
-     * @return 单条数据
-     */
-    @RequestMapping(value = "/updateById", method = RequestMethod.POST)
-    @ApiOperation(value = "通过主键更新一个**的接口",notes = "通过主键更新一个**的接口",httpMethod = "POST")
-    public Map<String, Object> updateById(@RequestBody @ApiParam(name = "userProfile",value = "pojo模型",required = true) UserProfile userProfile) {
-        return this.userProfileServiceImpl.updateById(userProfile);
-    }
-    
-    /**
-     * 通过一个pojo对象新增单条数据
-     *
-     * @param userProfile 一个数据库对应的POJO数据对象
-     * @return 返回插入的主键id
-     */
-    @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    @ApiOperation(value = "根据完整记录新增一个**的接口",notes = "根据完整记录新增一个**的接口",httpMethod = "POST")
-    public Map<String, Object> insert(@RequestBody @ApiParam(name = "userProfile",value = "pojo模型",required = true) UserProfile userProfile) {
-        return this.userProfileServiceImpl.insert(userProfile);
+        return new ModelAndView("user-profile");
     }
 
 }
